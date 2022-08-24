ad28cbfa7ca88aad8c653ba3f04ab7f2c1819d48
Jordan Keo <jkeough@45drives.com>
2022-08-24 16:52:19 -0300

    Removes old script files & improves functionality

--- benchmark/src/App.vue
+++ benchmark/src/App.vue
@@ -1,9 +1,6 @@
 <template>
-  <!--Benchmark Criteria Form-->
   <div id="benchmarkForm" class="centered-column w-full gap-content flex flex-col items-stretch text-left">
     <label class="text-header">Benchmarks</label>
-
-    <!--Error Alerts-->
 
     <div>
       <label class="text-label">Benchmark Tool</label>
@@ -11,7 +8,6 @@
         <option value="fio" selected>FIO</option>
       </select>
     </div>
-
 
     <div>
       <label class="text-label">Benchmark Type</label>
@@ -25,7 +21,8 @@
     <div>
       <label class="text-label mr-2">File Size</label>
       <div class="relative rounded-md shadow-sm inline w-full">
-        <input id="fileSize" v-model="fileSize" type="text" class="pr-12 input-textlike w-full sm:w-auto" />
+        <input @change="fileSizeNum()" id="fileSize" v-model="fileSize" type="text"
+          class="pr-12 input-textlike w-full sm:w-auto" />
         <div class="absolute inset-y-0 right-0 flex items-center">
           <label class="sr-only">Unit</label>
           <select id="fileSizeUnit" v-model="fileSizeUnit" class="input-textlike border-transparent bg-transparent">
@@ -34,9 +31,8 @@
           </select>
         </div>
       </div>
+      <p class="text-danger" v-if="fileSizeNumFeedback">{{ fileSizeNumFeedback }}</p>
     </div>
-
-    <!-- <div>FileSizeTest={{ testSize }}</div> -->
 
     <div>
       <label class="text-label mr-2">IO Depth</label>
@@ -54,41 +50,50 @@
 
     <div>
       <label class="text-label mr-2">Runtime</label>
-      <input id="runtime" v-model="runtime" type="number" class="input-textlike" default="2" />
-
-    </div>
-
-    <div id="6">
-      <label class="text-label mr-2">Test Path</label>
-      <input @change="validateInputs()" id="testPath" v-model="testPath" type="text" class="input-textlike"
-        placeholder="/mnt/hdd" />
-      <p v-if="testPathFeedback">{{ testPathFeedback }}</p>
+      <input @change="runtimeCheck()" id="runtime" v-model="runtime" type="number" class="input-textlike" default="2" />
+      <p class="text-danger" v-if="runtimeFeedback">{{ runtimeFeedback }}</p>
     </div>
 
     <div>
-      <button id="launchBenchmarkBtn" class="btn btn-primary mt-2 mr-2" @click="button()"
-        :disabled="!inputsValid">Launch</button>
-      <button id="downloadBenchmarkBtn" class="btn btn-primary mt-2 ml-2" disabled>Download Report</button>
-      <!-- <div id="spinner"
-        class="aspect-square animate-spin border-neutral-300 border-t-neutral-500 dark:border-neutral-500 dark:border-t-neutral-200 rounded-full hidden" /> -->
+      <label class="text-label mr-2">Test Path</label>
+      <input @change="validateFilePath()" id="testPath" v-model="testPath" type="text" class="input-textlike"
+        placeholder="/mnt/hdd" />
+      <p class="text-danger" v-if="testPathFeedback">{{ testPathFeedback }}</p>
+    </div>
 
-      <div id="benchmarkProgress" class="progress-container progress-description-left mt-4 hidden">
+    <div>
+      <button id="launchBenchmarkBtn" class="btn btn-primary mt-2 mr-2" @click="launchTests()"
+        :disabled="!pathValid || testInProgress || !numberValid || !runtimeValid">Launch</button>
+      <button id="downloadBenchmarkBtn" class="btn btn-primary mt-2 ml-2"
+        :disabled="testInProgress || !testCompleted">Download
+        Report</button>
+      <div ref="benchProg" class="progress-container progress-description-left mt-4" :class="{ hidden: hiddenClass1 }">
         <div class="progress-description">
-          <div class="bg-gray-200 rounded-full overflow-hidden">
-            <div class="h-2 bg-green-600 rounded-full" :style="{ width: `${progPercent}%` }"></div>
+          <div class="flex justify-between mb-1">
+            <span ref="benchStatus" class="text-secondary float-left" :class="{ hidden: hiddenClass1 }">{{
+                benchmarkFeedback
+            }}</span>
+            <span ref="benchPercent" class="text-secondary float-right" :class="{ hidden: hiddenClass1 }">{{
+                progPercent.toFixed(1)
+            }}%</span>
+          </div>
+          <div class="bg-gray-200 rounded-full overflow-hidden" :class="{ hidden: hiddenClass1 }">
+            <div class="h-3 bg-slate-500 rounded-full" :style="{ width: `${progPercent}%` }">
+            </div>
           </div>
         </div>
       </div>
-      <div id="benchmarkOutput" class="benchmark-output mt-6 text-center hidden float-left">
+      <div ref="benchOut" class="benchmark-output mt-6 text-center float-left" :class="{ hidden: hiddenClass2 }">
         <label class="text-label mr-2">Download Format</label>
         <select id="downloadFormat" v-model="downloadFormat" class="input-textlike bg-transparent">
           <option value="xlsx">XLSX</option>
           <option value="csv">CSV</option>
           <option value="ods">ODS</option>
         </select>
-        <div id="output" class="hidden float-right">
+        <div ref="chartOut" class=" float-right" :class="{ hidden: hiddenClass2 }">
           <label class="text-label mr-2">Output</label>
-          <select id="chartType" v-model="chartType" class="switcher input-textlike bg-transparent hidden">
+          <select ref="chartOut" v-model="chartType" class="switcher input-textlike bg-transparent"
+            :class="{ hidden: hiddenClass2 }">
             <option id="chart-type-iops" value="iops" selected>IOPS</option>
             <option id="chart-type-bandwidth" value="bandwith">Bandwidth</option>
           </select>
@@ -107,7 +112,6 @@ import { useSpawn } from "@45drives/cockpit-helpers";
 import { ref, computed } from "vue";
 import mergeDeep from "./assignObjectRecursive";
 
-//Input ->  Tool, Type, Size + Unit, IODepth, Runtime, Path
 const benchmarkTool = ref('fio');
 const benchmarkType = ref('throughput');
 const fileSize = ref(1);
@@ -115,55 +119,78 @@ const fileSizeUnit = ref(1024 ** 3);
 const ioDepth = ref('16');
 const runtime = ref('2');
 const testPath = ref('');
-
 const testSize = computed(() => fileSize.value * fileSizeUnit.value);
+
 const progPercent = ref(0);
 const downloadFormat = ref("xlsx");
 const chartType = ref("iops");
-const inputsValid = ref(true);
-const testPathFeedback = ref('');
 
-async function validateInputs() {
+const pathValid = ref(true);
+const numberValid = ref(true);
+const runtimeValid = ref(true);
+const testPathFeedback = ref('');
+const fileSizeNumFeedback = ref('');
+const benchmarkFeedback = ref('');
+const runtimeFeedback = ref('');
+
+const testInProgress = ref(false);
+const testCompleted = ref(false);
+const hiddenClass1 = ref(true);
+const hiddenClass2 = ref(true);
+
+const benchProg = ref('');
+const benchStatus = ref('');
+const benchPercent = ref('');
+const benchOut = ref('');
+const chartOut = ref('');
+
+
+async function validateFilePath() {
   let result = true;
   testPathFeedback.value = '';
   if (!(await checkIfExists(testPath.value))) {
     result = false;
     testPathFeedback.value = 'Path does not exist';
   }
-  inputsValid.value = result;
+  pathValid.value = result;
 }
-
-//LaunchBTN -> @click -> launchFunction -> Validate fields (fileSize, runtime, testPath)
-
-//showing output and progress bars for debugging
-function button() {
-  let benchOut = document.getElementById("benchmarkOutput");
-  let benchProg = document.getElementById("benchmarkProgress");
-  // let spin = document.getElementById("spinner");
-  let out = document.getElementById("output");
-  let chart = document.getElementById("chartType");
-
-  benchOut.classList.remove('hidden');
-  benchProg.classList.remove('hidden');
-  // spin.classList.remove('hidden');
-  out.classList.remove('hidden');
-  chart.classList.remove('hidden');
-
-
-  launchTests();
-}
-
 
 const checkIfExists = async (path) => {
   try {
     await useSpawn(['test', '-d', path], { superuser: 'try' }).promise();
     return true;
-
   } catch (error) {
     return false;
   }
 };
 
+function fileSizeNum() {
+  let result = true;
+  fileSizeNumFeedback.value = '';
+  if (isNaN(fileSize.value)) {
+    result = false;
+    fileSizeNumFeedback.value = 'Please enter numeric values only';
+  }
+  numberValid.value = result;
+}
+
+function runtimeCheck() {
+  let result = true;
+  runtimeFeedback.value = '';
+  if (runtime.value == 0 || isNaN(runtime.value)) {
+    result = false;
+    runtimeFeedback.value = 'Please enter a valid runtime';
+  }
+  runtimeValid.value = result;
+}
+
+const deleteFiles = async (file) => {
+  try {
+    await useSpawn(['sh', '-c', `rm -f ${testPath.value}/${file}`]).promise();
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 async function runFioJob({
   threadCount, recordSize, fileName, fileSize, testPath, ioDepth, runtime, testType
@@ -219,37 +246,6 @@ async function runFioJob({
   } catch (error) {
     console.error(error);
   }
-
-}
-
-async function launchTests() {
-  const testTypes = ['write', 'read', 'randread', 'randwrite'];
-  const results = {};
-  let recordSizes = [];
-  if (benchmarkType.value == 'throughput') {
-    recordSizes.push('1M');
-  }
-  else if (benchmarkType.value == 'iops') {
-    recordSizes.push('4k');
-  } else if (benchmarkType.value == 'spectrum') {
-    recordSizes.push('4k', '8k', '16k', '32k', '64k', '128k', '512k', '1M');
-  }
-  progPercent.value = 0;
-  for (const testType of testTypes) {
-    const result = await runFioJobs({
-      threadCount: 1,
-      recordSizes,
-      fileName: 'fiotest',
-      fileSize: testSize.value,
-      testPath: testPath.value,
-      ioDepth: ioDepth.value,
-      runtime: runtime.value,
-      testType,
-    }, testTypes.length)
-
-    mergeDeep(results, result);
-  }
-  console.log(results);
 }
 
 async function runFioJobs({
@@ -269,14 +265,54 @@ async function runFioJobs({
   return results;
 }
 
+async function launchTests() {
+  hiddenClass2.value = true;
+  hiddenClass1.value = false;
+  testCompleted.value = false;
+  testInProgress.value = true;
+  benchmarkFeedback.value = 'Testing...';
+
+  const testTypes = ['write', 'read', 'randread', 'randwrite'];
+  const results = {};
+  let recordSizes = [];
+  const fileName = 'FIO-Benchmark';
+  if (benchmarkType.value == 'throughput') {
+    recordSizes.push('1M');
+  }
+  else if (benchmarkType.value == 'iops') {
+    recordSizes.push('4k');
+  } else if (benchmarkType.value == 'spectrum') {
+    recordSizes.push('4k', '8k', '16k', '32k', '64k', '128k', '512k', '1M');
+  }
+  progPercent.value = 0;
+  for (const testType of testTypes) {
+    const result = await runFioJobs({
+      threadCount: 1,
+      recordSizes,
+      fileName,
+      fileSize: testSize.value,
+      testPath: testPath.value,
+      ioDepth: ioDepth.value,
+      runtime: runtime.value,
+      testType,
+    }, testTypes.length)
+
+    mergeDeep(results, result);
+  }
+  let file = fileName + '.0.0';
+  console.log(results);
+
+  benchmarkFeedback.value = 'Testing completed.';
+  testInProgress.value = false;
+  testCompleted.value = true;
+  hiddenClass2.value = false;
+
+  await deleteFiles(file);
+}
 
 //Export to chart -> XLSX/CSV/ODS
 
-
-
 //Download Chart
-
-
 
 </script>
 
