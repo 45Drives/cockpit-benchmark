edae3b9258af698cae24af5f95cc736f0cbaa918
JordanKeo45 <jkeough@45drives.com>
2024-05-14 15:26:28 -0300

    reverted minify code typo

--- benchmark/vite.config.js
+++ benchmark/vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
   plugins: [vue()],
   base: "./",
-  build: {
-    minify: false,
-  }
+  // build: {
+  //   minify: false,
+  // }
 })
